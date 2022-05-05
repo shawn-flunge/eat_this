17fc058f9719a9dd4c94a4cd3c7232cbf80daef6
shawn-flunge <echun1234@gmail.com>
2022-05-05 16:09:27 +0900

    google map, geolocator 추가

--- ios/Runner/Storage.swift
+++ ios/Runner/Storage.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct Storage{
+    let googleMapApiKey = "AIzaSyD1o5g6yXKIzXeq9XdzzEcA6EH338lgidE"
+}
--- lib/src/presentation/home_screen/home_screen.dart
+++ lib/src/presentation/home_screen/home_screen.dart
@@ -1,6 +1,12 @@
 
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+
+import 'dart:math' as math;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,6 +16,96 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    temp();
+  }
+
+  temp() async{
+    bool enabled = await Geolocator.isLocationServiceEnabled();
+    if(!enabled){
+      print('service disabled');
+    }
+    LocationPermission permission = await Geolocator.checkPermission();
+    if(permission == LocationPermission.denied){
+      print('permission denied');
+    }
+
+  }
+
+  _degreeToRadian(double degree){
+    return degree * math.pi / 180;
+  }
+  _radianToDegree(double radian){
+    return radian * 180 / math.pi;
+  }
+
+
+  double distanceBetween(double startLatitude, double startLongitude, double endLatitude, double endLongitude,) {
+    var earthRadius = 6378137.0;
+    var dLat = _degreeToRadian(endLatitude - startLatitude);
+    var dLon = _degreeToRadian(endLongitude - startLongitude);
+
+    var a = math.pow(math.sin(dLat / 2), 2) +
+        math.pow(math.sin(dLon / 2), 2) *
+            math.cos(_degreeToRadian(startLatitude)) *
+            math.cos(_degreeToRadian(endLatitude));
+
+    var c = 2 * math.asin(math.sqrt(a));
+
+    return earthRadius * c;
+  }
+
+  getLatLngsByDistance(double distance){
+
+    const double angle1 = 315;
+    const double angle2 = 135;
+  }
+
+
+  temp1() async{
+    LatLngBounds? latLng = await _googleMapController?.getVisibleRegion();
+
+    print('bdfbdfbdfbdf $latLng');
+    print(distanceBetween(37.524570661913515, 126.93301364779471, 37.527973900886344, 126.93722706288098));
+  }
+
+
+  getCurrentPosition() async{
+    // Geolocator.requestPermission();
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position.toString());
+
+    
+    setState(() {
+      LatLng newLatLng = LatLng(position.latitude, position.longitude);
+      _googleMapController?.moveCamera(CameraUpdate.newLatLng(newLatLng));
+    });
+  }
+
+  LatLng latLng = const LatLng(37.52627236692194, 126.93512036745244);
+  GoogleMapController? _googleMapController;
+
+  void _onMapCreated(GoogleMapController controller){
+    _googleMapController = controller;
+  }
+
+  void _onTap(LatLng latLng) async{
+
+    // Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    //
+    // final double distance = distanceBetween(position.latitude, position.longitude, latLng.latitude, latLng.longitude);
+    // print(distance);
+    // temp1();
+    temp();
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,14 +114,31 @@ class _HomeScreenState extends State<HomeScreen> {
           '이거 먹어'
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          // temp();
+          getCurrentPosition();
+        },
+      ),
       body: Column(
         children: <Widget>[
-
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.red,
+          SizedBox(
+            width: double.infinity,
+            height: 400,
+            child: GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: latLng,
+                tilt: 0,
+                zoom: 17,
+                bearing: 0,
+              ),
+              buildingsEnabled: false,
+              myLocationEnabled: true,
+              onMapCreated: _onMapCreated,
+              onTap: _onTap,
+            ),
           )
+
         ],
       ),
     );
