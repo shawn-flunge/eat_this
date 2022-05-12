0a2dca8557c98818236eda21e78766daa7c4ff42
shawn-flunge <echun1234@gmail.com>
2022-05-12 23:12:46 +0900

    add DI method and call it in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:eat_this/src/configuration/dependencies/repositories_injection.dart';
 import 'package:eat_this/src/eat_this_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  repositoriesInjection();
   runApp(const EatThisApp());
 }
