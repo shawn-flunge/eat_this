eba96f6c9028be2392d362dfc33d18648112e7ac
shawn-flunge <echun1234@gmail.com>
2022-05-26 23:18:37 +0900

    앱 시작전 di 수행

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
-import 'package:eat_this/src/configuration/dependencies/repositories_injection.dart';
+
+import 'package:eat_this/src/configuration/initializations/controllers.dart';
+import 'package:eat_this/src/configuration/initializations/repositories.dart';
+import 'package:eat_this/src/configuration/initializations/screen_controllers.dart';
 import 'package:eat_this/src/eat_this_app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   repositoriesInjection();
+  await initControllers();
+  await initScreenControllers();
   runApp(const EatThisApp());
 }
