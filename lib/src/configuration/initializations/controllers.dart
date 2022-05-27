1f1409381c93cce019cfc3f56906eacc606e1058
shawn-flunge <echun1234@gmail.com>
2022-05-28 00:39:54 +0900

    restaurant등 위치 정보 관리하는 컨트롤러 생성

--- lib/src/configuration/initializations/controllers.dart
+++ lib/src/configuration/initializations/controllers.dart
@@ -1,8 +1,10 @@
 
 
+import 'package:eat_this/src/domain/controllers/place_controller.dart';
 import 'package:eat_this/src/domain/controllers/user_location_controller.dart';
 
 Future<void> initControllers() async{
 
   await UserLocationController().init();
+  await PlaceController().init();
 }
---
+++ lib/src/domain/controllers/place_controller.dart
@@ -0,0 +1,37 @@
+
+
+import 'dart:async';
+
+import 'package:eat_this/src/data/models/restaurant.dart';
+import 'package:eat_this/src/data/repositories/restaurant_repository_impl.dart';
+
+class PlaceController{
+
+  static final PlaceController _instance = PlaceController._internal();
+  PlaceController._internal();
+
+  factory PlaceController(){
+    return _instance;
+  }
+
+  late RestaurantRepositoryImpl _restaurantRepositoryImpl;
+
+  init() async{
+    _restaurantRepositoryImpl = RestaurantRepositoryImpl();
+    // restaurants = StreamController<List<Restaurant>>();
+  }
+
+  // late StreamController<List<Restaurant>> restaurants;
+  List<Restaurant> restaurants = [];
+
+  Future<List<Restaurant>> getRestaurants({required double lat, required double lng, int radius = 500}) async{
+    return await _restaurantRepositoryImpl.getRestaurantsWithinCircle(lat: lat, lng: lng, radius: radius);
+  }
+
+  Future<void> setRestaurants ({required double lat, required double lng, int radius = 500}) async{
+    restaurants = await _restaurantRepositoryImpl.getRestaurantsWithinCircle(lat: lat, lng: lng, radius: radius);
+  }
+
+
+
+}
